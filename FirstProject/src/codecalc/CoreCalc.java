1793fa2ac8fd96226b472aadf6fd055924420a23
Rajeshwari.Gopalakrishnan@cognizant.com <Rajeshwari.Gopalakrishnan@cognizant.com>
2018-01-30 17:01:15 +0530

    Committed

---
+++ FirstProject/src/codecalc/CoreCalc.java
@@ -0,0 +1,11 @@
+package codecalc;
+
+public class CoreCalc {
+
+		public int add(int a,int b)
+		{
+			System.out.println("this is my new line");
+			int result=a+b;
+			return result;
+		}
+}
---
+++ UseJar/src/TestJar.java
@@ -0,0 +1,8 @@
+import codecalc.CoreCalc;
+public class TestJar {
+	private void Main() {
+		// TODO Auto-generated method stub
+		CoreCalc c=new CoreCalc();
+		System.out.println(c.add(5, 6));
+	}
+}
